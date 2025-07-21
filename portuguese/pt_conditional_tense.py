24e0c920d01f8bb0cf7069b2e41a7b28f0669e4a
Megan Jones <38929577+meginks@users.noreply.github.com>
2025-07-21 00:08:14 -0700

    portuguese updates

---
+++ portuguese/pt_conditional_tense.py
@@ -0,0 +1,43 @@
+
+# CONDITIONAL 
+
+def eu_conditional(infinitive):
+    return 
+
+def tu_conditional(infinitive):
+    return 
+
+def voce_ele_ela_conditional(infinitive): 
+    return 
+
+def nos_conditional(infinitive): 
+    return 
+
+def vos_conditional(infinitive): 
+    return 
+
+def voces_eles_elas_conditional(infinitive): 
+    return 
+
+
+
+# CONDITIONAL PERFECT 
+
+def eu_conditional_perfect(infinitive):
+    eu_conditional(infinitive)
+    return 
+
+def tu_conditional_perfect(infinitive):
+    return 
+
+def voce_ele_ela_conditional_perfect(infinitive): 
+    return 
+
+def nos_conditional_perfect(infinitive): 
+    return 
+
+def vos_conditional_perfect(infinitive): 
+    return 
+
+def voces_eles_elas_conditional_perfect(infinitive): 
+    return 
---
+++ portuguese/pt_past_tense.py
@@ -0,0 +1,61 @@
+# PRETERIT INDICATIVE 
+
+def eu_preterit(infinitive):
+    return 
+
+def tu_preterit(infinitive):
+    return 
+
+def voce_ele_ela_preterit(infinitive): 
+    return 
+
+def nos_preterit(infinitive): 
+    return 
+
+def vos_preterit(infinitive): 
+    return 
+
+def voces_eles_elas_preterit(infinitive): 
+    return  
+
+
+# IMPERFECT INDICATIVE 
+
+def eu_imperfect(infinitive):
+    return 
+
+def tu_imperfect(infinitive):
+    return 
+
+def voce_ele_ela_imperfect(infinitive): 
+    return 
+
+def nos_imperfect(infinitive): 
+    return 
+
+def vos_imperfect(infinitive): 
+    return 
+
+def voces_eles_elas_imperfect(infinitive): 
+    return  
+
+
+# SIMPLE PLUPERFECT INDICATIVE 
+
+def eu_simple_pluperfect(infinitive):
+    return 
+
+def tu_simple_pluperfect(infinitive):
+    return 
+
+def voce_ele_ela_simple_pluperfect(infinitive): 
+    return 
+
+def nos_simple_pluperfect(infinitive): 
+    return 
+
+def vos_simple_pluperfect(infinitive): 
+    return 
+
+def voces_eles_elas_simple_pluperfect(infinitive): 
+    return  
