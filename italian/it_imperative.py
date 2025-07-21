ff86a56c09339ead5231c65ee79d90250bcb8faf
Megan Jones <38929577+meginks@users.noreply.github.com>
2025-07-21 00:08:05 -0700

    italian updates

---
+++ italian/it_imperative.py
@@ -0,0 +1,17 @@
+## IMPERATIVO 
+
+def tu_imperativo(infinitive): 
+    return 
+
+def lui_lei_imperativo(infinitive): 
+    return 
+
+def noi_imperativo(infinitive):
+    return 
+
+def voi_imperativo(infinitive): 
+    return 
+
+def loro_imperativo(infinitive): 
+    return
+
--- italian/it_present_tense.py
+++ italian/it_present_tense.py
@@ -1,6 +1,9 @@
 from italian.it_verbs import * 
 from italian.it_helper_functions import *
 
+
+## PRESENTE 
+
 def io_present(infinitive): 
     return verbs[infinitive][0]  
 
@@ -33,4 +36,25 @@ def loro_present(infinitive):
 
 ########### CONGIUNTIVO #########################
 ## TO DO ## 
-## congiuntivo presente 
+## CONGIUNTIVO PRESENTE 
+
+def io_tu_lui_lei_congiuntivo_presente(infinitive): 
+    verb_ending = get_infinitive_ending(infinitive) 
+    conjugated_form = get_tu_stem(infinitive) 
+    if verb_ending == 'are': 
+        conjugated_form += 'i' 
+    elif verb_ending == 'ere' or verb_ending == 'ire': 
+        conjugated_form += 'a'
+    return conjugated_form
+
+# TO DO HANDLE NOI
+def noi_congiuntivo_presente(infinitive): 
+    return 
+
+# TO DO HANDLE VOI 
+def voi_congiuntivo_presente(infinitive): 
+    return 
+
+# TO DO HANDLE LORO 
+def loro_congiuntivo_presente(infinitive): 
+    return 
