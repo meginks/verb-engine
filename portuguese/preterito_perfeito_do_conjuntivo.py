0c9c48fbf3c685a68f6fb0bc56f8f90639631112
Megan Jones <38929577+meginks@users.noreply.github.com>
2026-02-21 11:22:11 -0600

    added preterito perfeito do conjuntivo

--- portuguese/preterito_perfeito_do_conjuntivo.py
+++ portuguese/preterito_perfeito_do_conjuntivo.py
@@ -1,20 +1,44 @@
+from pt_helper_functions import * 
+from pt_verbs import * 
+from pt_rule_exceptions import * 
+
 def eu_preterito_perfeito_conjuntivo(infinitive):
-    return 
+    conjugated_forms = ''
+    for form in verbs[infinitive][4]: 
+        conjugated_forms += 'tenha ' + form + '\n'
+    return conjugated_forms
 
 def tu_preterito_perfeito_conjuntivo(infinitive):
-    return 
+    conjugated_forms = ''
+    for form in verbs[infinitive][4]: 
+        conjugated_forms += 'tenhas ' + form + '\n'
+    return conjugated_forms
 
 def voce_ele_ela_preterito_perfeito_conjuntivo(infinitive): 
-    return 
+    return eu_preterito_perfeito_conjuntivo(infinitive) 
 
 def nos_preterito_perfeito_conjuntivo(infinitive): 
-    return 
+    conjugated_forms = ''
+    for form in verbs[infinitive][4]: 
+        conjugated_forms += 'tenhamos ' + form + '\n'
+    return conjugated_forms
 
 def vos_preterito_perfeito_conjuntivo(infinitive): 
-    return 
+    conjugated_forms = ''
+    for form in verbs[infinitive][4]: 
+        conjugated_forms += 'tenhais ' + form + '\n'
+    return conjugated_forms
 
 def voces_eles_elas_preterito_perfeito_conjuntivo(infinitive): 
-    return  
+    conjugated_forms = ''
+    for form in verbs[infinitive][4]: 
+        conjugated_forms += 'tenham ' + form + '\n'
+    return conjugated_forms
 
 def make_preterito_perfeito_do_conjuntivo(infinitive): 
-    return
+    return (eu_preterito_perfeito_conjuntivo(infinitive),
+            tu_preterito_perfeito_conjuntivo(infinitive),
+            voce_ele_ela_preterito_perfeito_conjuntivo(infinitive),
+            nos_preterito_perfeito_conjuntivo(infinitive),
+            vos_preterito_perfeito_conjuntivo(infinitive),
+            voces_eles_elas_preterito_perfeito_conjuntivo(infinitive))  
