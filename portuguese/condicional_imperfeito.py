a740d497e91dac5dbcbb7ce09150d51305314905
Megan Jones <38929577+meginks@users.noreply.github.com>
2026-02-21 11:03:05 -0600

    added condicional imperfeito

--- portuguese/condicional_imperfeito.py
+++ portuguese/condicional_imperfeito.py
@@ -1,21 +1,26 @@
 
-def eu_condicional(infinitive):
-    return 
+def eu_condicional_imperfeito(infinitive):
+    return infinitive + 'ia'
 
-def tu_condicional(infinitive):
-    return 
+def tu_condicional_imperfeito(infinitive):
+    return infinitive + 'ias'
 
-def voce_ele_ela_condicional(infinitive): 
-    return 
+def voce_ele_ela_condicional_imperfeito(infinitive): 
+    return infinitive + 'ia'
 
-def nos_condicional(infinitive): 
-    return 
+def nos_condicional_imperfeito(infinitive): 
+    return infinitive + 'íamos'
 
-def vos_condicional(infinitive): 
-    return 
+def vos_condicional_imperfeito(infinitive): 
+    return infinitive + 'íeis'
 
-def voces_eles_elas_condicional(infinitive): 
-    return  
+def voces_eles_elas_condicional_imperfeito(infinitive): 
+    return infinitive + 'iam'
 
 def make_condicional_imperfeito(infinitive): 
-    return
+    return (eu_condicional_imperfeito(infinitive),
+            tu_condicional_imperfeito(infinitive),
+            voce_ele_ela_condicional_imperfeito(infinitive),
+            nos_condicional_imperfeito(infinitive),
+            vos_condicional_imperfeito(infinitive),
+            voces_eles_elas_condicional_imperfeito(infinitive))
