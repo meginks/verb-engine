bfb2549b56ba71d97d717437bdc9f749dabb4bdb
Megan Jones <38929577+meginks@users.noreply.github.com>
2026-02-21 11:15:21 -0600

    added futuro do conjuntivo

--- portuguese/futuro_do_conjuntivo.py
+++ portuguese/futuro_do_conjuntivo.py
@@ -1,20 +1,29 @@
+from pt_helper_functions import * 
+from pt_verbs import * 
+from pt_rule_exceptions import * 
+
 def eu_futuro_congiuntivo(infinitive):
-    return 
+    return get_third_person_plural_preterite_stem(infinitive) + 'r'
 
 def tu_futuro_congiuntivo(infinitive):
-    return 
+    return get_third_person_plural_preterite_stem(infinitive) + 'res'
 
 def voce_ele_ela_futuro_congiuntivo(infinitive): 
-    return 
+    return get_third_person_plural_preterite_stem(infinitive) + 'r'
 
 def nos_futuro_congiuntivo(infinitive): 
-    return 
+    return get_third_person_plural_preterite_stem(infinitive) + 'rmos'
 
 def vos_futuro_congiuntivo(infinitive): 
-    return 
+    return get_third_person_plural_preterite_stem(infinitive) + 'rdes'
 
 def voces_eles_elas_futuro_congiuntivo(infinitive): 
-    return  
+    return  get_third_person_plural_preterite_stem(infinitive) + 'rem'
 
 def make_futuro_do_conjuntivo(infinitive): 
-    return
+    return (eu_futuro_congiuntivo(infinitive),
+            tu_futuro_congiuntivo(infinitive),
+            voce_ele_ela_futuro_congiuntivo(infinitive),
+            nos_futuro_congiuntivo(infinitive),
+            vos_futuro_congiuntivo(infinitive),
+            voces_eles_elas_futuro_congiuntivo(infinitive))  
