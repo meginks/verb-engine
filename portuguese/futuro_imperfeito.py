a9695919f2c65f7c3f6855d76c0357af3afdc0d3
Megan Jones <38929577+meginks@users.noreply.github.com>
2026-02-21 11:01:34 -0600

    added futuro imperfeito

--- portuguese/futuro_imperfeito.py
+++ portuguese/futuro_imperfeito.py
@@ -1,21 +1,29 @@
+from pt_verbs import * 
+from pt_helper_functions import * 
+from pt_rule_exceptions import * 
 
 def eu_futuro_imperfeito(infinitive):
-    return 
+    return infinitive + 'ei'
 
 def tu_futuro_imperfeito(infinitive):
-    return 
+    return infinitive + 'ás'
 
 def voce_ele_ela_futuro_imperfeito(infinitive): 
-    return 
+    return infinitive + 'á'
 
 def nos_futuro_imperfeito(infinitive): 
-    return 
+    return infinitive + 'emos'
 
 def vos_futuro_imperfeito(infinitive): 
-    return 
+    return infinitive + 'eis'
 
 def voces_eles_elas_futuro_imperfeito(infinitive): 
-    return  
+    return infinitive + 'ão'
 
 def make_futuro_imperfeito(infinitive): 
-    return
+    return (eu_futuro_imperfeito(infinitive),
+            tu_futuro_imperfeito(infinitive),
+            voce_ele_ela_futuro_imperfeito(infinitive),
+            nos_futuro_imperfeito(infinitive),
+            vos_futuro_imperfeito(infinitive),
+            voces_eles_elas_futuro_imperfeito(infinitive))
