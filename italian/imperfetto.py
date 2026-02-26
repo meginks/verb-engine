817cd95af066fd301d2b3bd5982b790d2b156ccd
Megan Jones <38929577+meginks@users.noreply.github.com>
2026-02-26 15:51:35 -0600

    added presente, passato remoto, e imperfetto

--- italian/imperfetto.py
+++ italian/imperfetto.py
@@ -1,23 +1,61 @@
+from it_helper_functions import *
+from it_verbs import * 
+
 ## IMPERFETTO  
 
 def io_imperfetto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'avo' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'evo' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'ivo' 
 
 def tu_imperfetto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'avi' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'evi' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'ivi' 
 
 def lui_lei_imperfetto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'ava' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'eva' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'iva' 
 
 def noi_imperfetto(infinitive):
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'avamo' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'evamo' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'ivamo' 
 
 def voi_imperfetto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'avate' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'evate' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'ivate' 
 
 def loro_imperfetto(infinitive): 
-    return
-
+    ending = get_infinitive_ending(infinitive) 
+    if ending == are: 
+        return get_infinitive_stem(infinitive) + 'avano' 
+    elif ending == ere or ending == rre: 
+        return get_infinitive_stem(infinitive) + 'evano' 
+    elif ending == ire:
+        return get_infinitive_stem(infinitive) + 'ivano' 
 
 # TO DO HANDLE IMPERFECT / imperfetto 
 def make_imperfetto(infinitive): 
--- italian/passato_remoto.py
+++ italian/passato_remoto.py
@@ -1,25 +1,81 @@
+from it_helper_functions import * 
 
+
+# NOTE I have not handled the case where -ere verbs (except -ttere and --ssere verbs) have 2 options for 1st and 3rd person singular and 3rd person plural, e.g. credere --> io credei / creddetti, lui credé / creddette, loro crederono / creddettero -- TODO  
 
 ## PASSATO REMOTO 
 
 def io_passato_remoto(infinitive): 
-
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'ai' 
+    elif ending == ere: 
+        return stem + 'ei' 
+    elif ending == ire: 
+        return stem + 'ii' 
+    elif ending == rre:  
+        return stem + 'ssi'
 
 def tu_passato_remoto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'asti' 
+    elif ending == ere: 
+        return stem + 'esti' 
+    elif ending == ire: 
+        return stem + 'isti' 
+    elif ending == rre:  
+        return get_third_person_plural_present_stem(infinitive)+ 'esti'
 
 def lui_lei_passato_remoto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'ò' 
+    elif ending == ere: 
+        return stem + 'è' 
+    elif ending == ire: 
+        return stem + 'ì' 
+    elif ending == rre:  
+        return stem + 'sse'
 
 def noi_passato_remoto(infinitive):
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'ammo' 
+    elif ending == ere: 
+        return stem + 'emmo' 
+    elif ending == ire: 
+        return stem + 'immo' 
+    elif ending == rre:  
+        return get_third_person_plural_present_stem(infinitive) + 'emmo'
 
 def voi_passato_remoto(infinitive): 
-    return 
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'aste' 
+    elif ending == ere: 
+        return stem + 'este' 
+    elif ending == ire: 
+        return stem + 'iste' 
+    elif ending == rre:  
+        return get_third_person_plural_present_stem(infinitive) + 'este'
 
 def loro_passato_remoto(infinitive): 
-    return
+    ending = get_infinitive_ending(infinitive) 
+    stem = get_infinitive_stem(infinitive) 
+    if ending == are: 
+        return stem + 'arono' 
+    elif ending == ere: 
+        return stem + 'erono' 
+    elif ending == ire: 
+        return stem + 'irono' 
+    elif ending == rre:  
+        return stem + 'ssero'
 
 def make_passato_remoto(infinitive): 
     return (io_passato_remoto(infinitive),
